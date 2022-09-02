5e47a8d52ff34b93123ca0e376d4dfc345734afa
KaterinaOsipenko <kate.osipenko30@gmail.com>
2022-09-02 15:25:39 +0300

    refactor

--- src/main/java/com/spring/currency/bot/service/KeyboardServiceImpl.java
+++ src/main/java/com/spring/currency/bot/service/KeyboardServiceImpl.java
@@ -11,13 +11,9 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 @Slf4j
 @Service
