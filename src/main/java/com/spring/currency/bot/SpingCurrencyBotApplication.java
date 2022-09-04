c148d750cff4f12a78817821f4577fa62273b2ed
KaterinaOsipenko <kate.osipenko30@gmail.com>
2022-09-04 23:34:54 +0300

    system properties were added

--- src/main/java/com/spring/currency/bot/SpingCurrencyBotApplication.java
+++ src/main/java/com/spring/currency/bot/SpingCurrencyBotApplication.java
@@ -1,16 +1,26 @@
 package com.spring.currency.bot;
 
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCaching
+@Controller
 public class SpingCurrencyBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpingCurrencyBotApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String get() {
+        return "Hello";
     }
 
 }
