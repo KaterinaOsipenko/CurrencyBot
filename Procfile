4c78cbe7ac9ff5c50c55d8a56a9881d9984d5854
KaterinaOsipenko <kate.osipenko30@gmail.com>
2022-09-05 17:14:27 +0300

    system properties were added

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT -jar target/sping-currency-bot-0.0.1-SNAPSHOT.jar
--- src/main/java/com/spring/currency/bot/WebhookController.java
+++ src/main/java/com/spring/currency/bot/WebhookController.java
@@ -2,10 +2,8 @@ package com.spring.currency.bot;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -19,7 +17,7 @@ public class WebhookController {
 
   @PostMapping("/")
   public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
-    log.info("WebhookController: controller updating by id: " + update.getUpdateId());
+    log.info("WebhookController: controller updating by id:" + update.getUpdateId());
     return telegramBot.onWebhookUpdateReceived(update);
   }
 
