b60715b18e20a50c13281f7a20890d625f3777a1
KaterinaOsipenko <kate.osipenko30@gmail.com>
2022-09-06 15:24:16 +0300

    heroku

--- src/main/java/com/spring/currency/bot/WebhookController.java
+++ src/main/java/com/spring/currency/bot/WebhookController.java
@@ -2,6 +2,9 @@ package com.spring.currency.bot;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +24,8 @@ public class WebhookController {
     return telegramBot.onWebhookUpdateReceived(update);
   }
 
+  @GetMapping("/")
+  public ResponseEntity<?> onUpdateReceived() {
+    return new ResponseEntity<>(HttpStatus.OK, HttpStatus.valueOf("OK"));
+  }
 }
